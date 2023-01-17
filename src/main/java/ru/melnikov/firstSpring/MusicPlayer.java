f9a2cdaf0557fd549d10cd88ec56073cac20afb8
Nickolas990 <hrom_90@hotmail.com>
2023-01-17 19:17:19 +0300

    Solved task with injecting ArrayList

--- src/main/java/ru/melnikov/firstSpring/MusicPlayer.java
+++ src/main/java/ru/melnikov/firstSpring/MusicPlayer.java
@@ -1,12 +1,23 @@
 package ru.melnikov.firstSpring;
 
-public class MusicPlayer {
-    private Music music;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-    public MusicPlayer(Music music) {
-        this.music = music;
-    }
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class MusicPlayer {
+    private List<Music> musicList = new ArrayList<>();
+    private String name;
+    private int volume;
+
     public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+        musicList.forEach(music -> System.out.println("Playing: " + music.getSong()));
     }
 }
